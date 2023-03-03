ff93fda7bc06f2c938edcd076e407d5ee20afbdb
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2023-03-03 16:05:14 +0530

    Improve certificate creation

--- tests/certificate/index.ts
+++ tests/certificate/index.ts
@@ -1,16 +1,51 @@
-import { handler } from "../../src/handler";
-import { event } from "./event.mock";
 import * as fs from "fs";
 import { join } from "path";
+import { handler } from "../../src/handler";
+
+class CertificateBuilder {
+  template = "certificate";
+  payload = {
+    name: "",
+    teacher: "Albus Dumbledore",
+    date: "March 03, 2023",
+  };
+
+  constructor(name: string) {
+    this.payload.name = name;
+  }
+}
+
+const names = [
+  "Harry Potter",
+  "Ron Weasley",
+  "Hermione Granger",
+  "Neville Longbottom",
+  "Draco Malfoy",
+];
+
+const certificates: any[] = [];
+
+names.forEach((name) => {
+  certificates.push(new CertificateBuilder(name));
+});
 
 (async () => {
   let result;
   try {
-    result = await handler(event, {} as any);
+    const promises: any[] = [];
 
-    fs.writeFileSync(join(__dirname, "../../.temp/result"), result.data);
+    certificates.forEach((certificate) => {
+      const promise = handler(certificate, {} as any);
 
-    console.log(result);
+      promises.push(promise);
+    });
+
+    const result = await Promise.all(promises);
+
+    fs.writeFileSync(
+      join(__dirname, "../../.temp/last_file_base64"),
+      result[result.length - 1].data
+    );
   } catch (error) {
     console.error(error);
   }
